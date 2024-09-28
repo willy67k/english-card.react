c4b5db9ee7afdffc0b55ad46543fcdbef3e63229
Lilp Su <willy67k001@gmail.com>
2024-09-29 00:04:18 +0800

    fix: add vacabs

--- src/utils/vacabularyMaker/material.tsx
+++ src/utils/vacabularyMaker/material.tsx
@@ -1,7 +1,34 @@
 const str = `
-acclaim,noun,讚賞
-be committed with,phrase,專注於
-narrowly,adverb,勉強地
+sanitary,adjective,衛生的
+regardless,adverb,不管怎樣
+merchandise,noun,商品
+comply with,phrase,響應
+as a token of,phrase,為了表達
+goodwill,noun,善意
+pest,noun,害蟲
+apply a to b,phrase,讓a適用於b
+carry out,phrase,實踐
+in brief,phrase,簡而言之
+appealing(to),adjective,吸引力的
+intergrated,adjective,整合的
+patented,adjective,取得專利的
+warranty,noun,保固
+accommodate,verb,收容
+acquire,verb,收購
+municipal,adjective,市政的
+conserve,verb,節約
+conprehensive,adjective,包羅萬象的
+concise,adjective,精簡的
+on track,phrase,按部就班
+refferal,noun,推薦
+appropriate,adjective,適合的
+renovation,noun,整修
+relocation,noun,搬遷
+explict,adjective,明確的
+overdue,adjective,逾期的
+exceptional,adjective,卓越的
+distinctive,adjective,獨特的
+confidential,adjective,機密的
 `;
 str
   .split("\n")
